f1ef129b8de47aeb5d5c1b78c5f15e36213d68ce
Pranay Rampelly <pranay.rampelly431@gmail.com>
2026-06-21 01:48:21 +0530

    vercel analytics enabled

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@
  */
 
 import React from "react";
+import { Analytics } from "@vercel/analytics/react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
@@ -111,6 +112,9 @@ export default function App() {
           </span>
         </div>
       </footer>
+
+      {/* Vercel Web Analytics */}
+      <Analytics />
     </div>
   );
 }
